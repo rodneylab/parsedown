ae4973bea47f4196fca84b581d9ce668db2efa2e
Rodney Lab <codesign@rodneylab.com>
2022-12-19 07:09:13 +0000

    style: 🛁  update formatting

--- src/markdown/tests.rs
+++ src/markdown/tests.rs
@@ -3,15 +3,14 @@ use crate::markdown::slugified_title;
 #[test]
 pub fn test_slugified_title() {
     let title = "Heading One";
-    assert_eq!(slugified_title(title),"heading-one");
+    assert_eq!(slugified_title(title), "heading-one");
 
-        let title = "🌟 Heading Two";
-    assert_eq!(slugified_title(title),"*-heading-two");
+    let title = "🌟 Heading Two";
+    assert_eq!(slugified_title(title), "*-heading-two");
 
-        let title = "💫 Heading Three";
-    assert_eq!(slugified_title(title),"dizzy-heading-three");
+    let title = "💫 Heading Three";
+    assert_eq!(slugified_title(title), "dizzy-heading-three");
 
-        let title = "Heading Four!";
-    assert_eq!(slugified_title(title),"heading-four");
-
+    let title = "Heading Four!";
+    assert_eq!(slugified_title(title), "heading-four");
 }
