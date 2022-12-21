cff9c5d3e350d5bb9077737df3cb278550a815ae
Rodney Lab <codesign@rodneylab.com>
2022-12-21 07:22:35 +0000

    feat: 🌟 add mjml parsing to HTML

--- src/lib.rs
+++ src/lib.rs
@@ -28,6 +28,25 @@ pub fn markdown_to_html(markdown: &str) -> String {
     }
 }
 
+#[wasm_bindgen]
+pub fn mjml_to_html(mjml: &str) -> String {
+    let root = match mrml::parse(mjml) {
+        Ok(value) => value,
+        Err(error) => {
+            console_log!("Error parsing mjml: {:?}", error);
+            return String::from("");
+        }
+    };
+    let opts = mrml::prelude::render::Options::default();
+    match root.render(&opts) {
+        Ok(value) => value,
+        Err(error) => {
+            console_log!("Error rendering parsed mjml to html: {:?}", error);
+            String::from("")
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -43,15 +62,13 @@ hello
 "#;
 
         let result = markdown_to_html(markdown);
-        assert_eq!(
-            result,
-            r#"<h1 id="hello">hello</h1>
+        let expected = r#"<h1 id="hello">hello</h1>
 <ul>
 <li>alpha</li>
 <li>beta</li>
 </ul>
-"#
-        );
+"#;
+        assert_eq!(result, expected);
 
         let markdown = r#"
 ## Subheading
@@ -60,12 +77,10 @@ Paragraph text.
 "#;
 
         let result = markdown_to_html(markdown);
-        assert_eq!(
-            result,
-            r##"<h2 id="subheading">Subheading <a href="#subheading" class="heading-anchor">#</a></h2>
+        let expected = r##"<h2 id="subheading">Subheading <a href="#subheading" class="heading-anchor">#</a></h2>
 <p>Paragraph text.</p>
-"##
-        );
+"##;
+        assert_eq!(result, expected);
 
         let markdown = r#"
 ### Subheading
@@ -74,11 +89,40 @@ Link: [Example site](https://example.com).
 "#;
 
         let result = markdown_to_html(markdown);
-        assert_eq!(
-            result,
-            r##"<h3 id="subheading">Subheading</h3>
+        let expected = r##"<h3 id="subheading">Subheading</h3>
 <p>Link: <a href="https://example.com" rel="nofollow noopener noreferrer">Example site</a>.</p>
-"##
-        );
+"##;
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_mjml_to_html() {
+        let mjml = r#"<mjml></mjml>"#;
+        let result = mjml_to_html(mjml);
+        let expected = r#"<!doctype html><html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office"><head><title></title><!--[if !mso]><!--><meta http-equiv="X-UA-Compatible" content="IE=edge"><!--<![endif]--><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1">
+<style type="text/css">
+#outlook a { padding: 0; }
+body { margin: 0; padding: 0; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; }
+table, td { border-collapse: collapse; mso-table-lspace: 0pt; mso-table-rspace: 0pt; }
+img { border: 0; height: auto; line-height: 100%; outline: none; text-decoration: none; -ms-interpolation-mode: bicubic; }
+p { display: block; margin: 13px 0; }
+</style>
+<!--[if mso]>
+<noscript>
+<xml>
+<o:OfficeDocumentSettings>
+  <o:AllowPNG/>
+  <o:PixelsPerInch>96</o:PixelsPerInch>
+</o:OfficeDocumentSettings>
+</xml>
+</noscript>
+<![endif]-->
+<!--[if lte mso 11]>
+<style type="text/css">
+.mj-outlook-group-fix { width:100% !important; }
+</style>
+<![endif]-->
+</head><body></body></html>"#;
+        assert_eq!(result, expected);
     }
 }
