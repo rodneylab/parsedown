69638945cdc97c0fe32455600216ee75587390d8
Rodney Lab <codesign@rodneylab.com>
2022-12-21 08:34:02 +0000

    test: ☑️  update unit tests

--- src/html_process/tests.rs
+++ src/html_process/tests.rs
@@ -1,4 +1,29 @@
-use crate::html_process::relative_url;
+use crate::html_process::{process_html, relative_url};
+
+#[test]
+fn test_process_html() {
+    let result = process_html("<a href=\"https://example.com\">Example</a>");
+    let expected =
+        "<a href=\"https://example.com\" rel=\"nofollow noopener noreferrer\">Example</a>";
+    assert_eq!(result, expected);
+
+    let result = process_html("<a href=\"/pathname?utm=123#anchor\">Example</a>");
+    let expected = "<a href=\"/pathname?utm=123#anchor\">Example</a>";
+    assert_eq!(result, expected);
+
+    let result = process_html("<h2>Heading</h2>");
+    let expected = "<h2>Heading</h2>";
+    assert_eq!(result, expected);
+
+    let result = process_html("<h2 id=\"heading\">Heading</h2>");
+    let expected =
+        "<h2 id=\"heading\">Heading <a href=\"#heading\" class=\"heading-anchor\">#</a></h2>";
+    assert_eq!(result, expected);
+
+    let result = process_html("<h3 id=\"heading\">Heading</h3>");
+    let expected = "<h3 id=\"heading\">Heading</h3>";
+    assert_eq!(result, expected);
+}
 
 #[test]
 fn test_relative_url() {
