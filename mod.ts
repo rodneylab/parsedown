8ad1d545a7a185b56ce30d6c0e31d0b560ca66f7
Rodney Lab <codesign@rodneylab.com>
2023-01-12 17:33:49 +0000

    feat: 🌟 add Deno module

---
+++ mod.ts
@@ -0,0 +1,48 @@
+import init, {
+  markdown_to_html,
+  markdown_to_plaintext,
+  mjml_to_html,
+} from "lib/parsedown.generated.js";
+
+interface MarkdownToHtmlOKOutput {
+  errors?: never;
+  headings: { heading: string; id: string }[];
+  html: string;
+  statistics: {
+    reading_time: number;
+    word_count: number;
+  };
+}
+
+interface MarkdownToHtmlErrorOutput {
+  errors: string[];
+  headings?: never;
+  html?: never;
+  statistics?: never;
+}
+
+const markdownToHtml: (
+  markdown: string,
+  options?: { canonical_root_url?: string },
+) => Promise<MarkdownToHtmlOKOutput | MarkdownToHtmlErrorOutput> =
+  async function markdownToHtml(markdown, options) {
+    await init();
+    return markdown_to_html(markdown, options);
+  };
+
+const markdownToPlaintext: (
+  markdown: string,
+  options?: { canonical_root_url?: string },
+) => Promise<string> = async function markdownToPlaintext(markdown, options) {
+  await init();
+  return markdown_to_plaintext(markdown, options);
+};
+
+const mjmlToHtml: (mjml: string) => Promise<string> = async function mjmlToHtml(
+  mjml,
+) {
+  await init();
+  return mjml_to_html(mjml);
+};
+
+export { markdownToHtml, markdownToPlaintext, mjmlToHtml };
