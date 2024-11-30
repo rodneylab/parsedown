09745b50873d2d0175b2a7a2da10985f4904218e
Rodney Lab <codesign@rodneylab.com>
2024-11-30 08:53:01 +0000

    style: 🛁 linting

--- test/node.test.ts
+++ test/node.test.ts
@@ -1,71 +1,69 @@
 import {
-    markdown_to_html as markdownToHtml,
-    markdown_to_plaintext as markdownToPlaintext,
-    mjml_to_html as mjmlToHtml,
+  markdown_to_html as markdownToHtml,
+  markdown_to_plaintext as markdownToPlaintext,
+  mjml_to_html as mjmlToHtml,
 } from "@rodneylab/parsedown";
 import { assert, expect, test } from "vitest";
 
 test("it parses markdown to html", async () => {
-    // arrange
-    const markdown = `
+  // arrange
+  const markdown = `
 ## 👋🏽 Hello You
 
 * alpha
 * beta
 `;
 
-    // act
-    const { errors, html, headings, statistics } = (await markdownToHtml(
-        markdown,
-        {},
-    ))!;
+  // act
+  const { errors, html, headings, statistics } = (await markdownToHtml(
+    markdown,
+    {},
+  ))!;
 
-    // assert
-    assert(typeof markdownToHtml === "function");
+  // assert
+  assert(typeof markdownToHtml === "function");
 
-    expect(typeof errors).toBe("undefined");
+  expect(typeof errors).toBe("undefined");
 
-    const { reading_time, word_count } = statistics;
+  const { reading_time, word_count } = statistics;
 
-    assert(typeof headings !== "undefined");
-    expect(headings?.length).toBe(1);
-    expect(headings?.[0]).toStrictEqual({
-        heading: "👋🏽 Hello You",
-        id: "wave-hello-you",
-    });
-    expect(
-        html,
-    ).toBe(
-        `<h2 id="wave-hello-you">👋🏽 Hello You <a href="#wave-hello-you" class="heading-anchor">#</a></h2>
+  assert(typeof headings !== "undefined");
+  expect(headings?.length).toBe(1);
+  expect(headings?.[0]).toStrictEqual({
+    heading: "👋🏽 Hello You",
+    id: "wave-hello-you",
+  });
+  expect(html).toBe(
+    `<h2 id="wave-hello-you">👋🏽 Hello You <a href="#wave-hello-you" class="heading-anchor">#</a></h2>
 <ul>
 <li>alpha</li>
 <li>beta</li>
 </ul>
 `,
-    );
+  );
 
-    expect(reading_time).toBe(1);
-    expect(word_count).toBe(4);
+  expect(reading_time).toBe(1);
+  expect(word_count).toBe(4);
 });
 
 test("it highlights search terms in generated HTML", async () => {
-    // prepare
-    const markdown = "Nobody likes maple in their apple flavoured Snapple.";
-    // act
-    const { errors, html } = await markdownToHtml(markdown, {
-        search_term: "apple",
-    });
+  // prepare
+  const markdown = "Nobody likes maple in their apple flavoured Snapple.";
+  // act
+  const { errors, html } = await markdownToHtml(markdown, {
+    search_term: "apple",
+  });
 
-    // assert
-    assert(typeof errors === "undefined");
-    expect(html).toBe(
-        `<p>Nobody likes maple in their <mark id="search-match">apple</mark> flavoured Sn<mark>apple</mark>.</p>\n`,
-    );
+  // assert
+  assert(typeof errors === "undefined");
+  expect(html).toBe(
+    `<p>Nobody likes maple in their <mark id="search-match">apple</mark> flavoured Sn<mark>apple</mark>.</p>\n`,
+  );
 });
 
 test("it parses markdown to plain text", () => {
-    // arrange
-    const markdown = `
+  // arrange
+  const markdown = `
 ## 👋🏽 Hello You
 
 * alpha
@@ -74,15 +72,13 @@ test("it parses markdown to plain text", () => {
 [Example Link](https://example.com/)
 `;
 
-    // act
-    const plaintext = markdownToPlaintext(markdown, {});
+  // act
+  const plaintext = markdownToPlaintext(markdown, {});
 
-    // assert
-    assert(typeof markdownToPlaintext === "function");
-    expect(
-        plaintext,
-    ).toBe(
-        `👋🏽 Hello You
+  // assert
+  assert(typeof markdownToPlaintext === "function");
+  expect(plaintext).toBe(
+    `👋🏽 Hello You
 
 - alpha
 
@@ -90,22 +86,20 @@ test("it parses markdown to plain text", () => {
 
 Example Link (https://example.com/)
 `,
-    );
+  );
 });
 
 test("it parses mjml to html", () => {
-    // arrange
-    const mjml = "<mjml></mjml>";
+  // arrange
+  const mjml = "<mjml></mjml>";
 
-    // act
-    const html = mjmlToHtml(mjml);
+  // act
+  const html = mjmlToHtml(mjml);
 
-    // assert
-    assert(typeof mjmlToHtml === "function");
-    expect(
-        html,
-    ).toBe(
-        `<!doctype html><html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office"><head><title></title><!--[if !mso]><!--><meta http-equiv="X-UA-Compatible" content="IE=edge"><!--<![endif]--><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1">
+  // assert
+  assert(typeof mjmlToHtml === "function");
+  expect(html).toBe(
+    `<!doctype html><html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office"><head><title></title><!--[if !mso]><!--><meta http-equiv="X-UA-Compatible" content="IE=edge"><!--<![endif]--><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1">
 <style type="text/css">
 #outlook a { padding: 0; }
 body { margin: 0; padding: 0; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; }
@@ -129,5 +123,5 @@ p { display: block; margin: 13px 0; }
 </style>
 <![endif]-->
 <style type="text/css"></style></head><body></body></html>`,
-    );
+  );
 });
